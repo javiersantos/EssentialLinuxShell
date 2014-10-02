2412ab148d856f4a6d4be5226b72aa5d18ed04ea
Javier Santos <javier.santos.vel@gmail.com>
2014-10-02 21:24:11 +0200

    Improve requirements

--- essentiallinuxshell.sh
+++ essentiallinuxshell.sh
@@ -7,10 +7,10 @@ VERSION=20140930
 # CHOOSE LANGUAGE AND OPEN SH SCRIPT
 LANGUAGE=$(zenity --list --title="Choose your language" --text="Please select the language to use for the interface.\nPor favor, seleccione el idioma deseo para la interfaz.\n"  --column="Language" "English" "Español")
 if [ $LANGUAGE = "English" ]; then
-	./Ubuntu/14.04/EN/requirements.sh
+	./bin/EN/requirements.sh
 	#echo "English"
 fi
 if [ $LANGUAGE = "Español" ]; then
-	./Ubuntu/14.04/ES/requirements.sh
+	./bin/ES/requirements.sh
 	#echo "Español"
 fi
